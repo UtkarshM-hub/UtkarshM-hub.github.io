97f729c5c00709e4f7949c4bcf64052bd6bbf28d
UtkarshM-hub <70505181+UtkarshM-hub@users.noreply.github.com>
2021-06-15 08:36:37 -0400

    NCC website project

---
+++ project/index.js
@@ -0,0 +1,36 @@
+// console.log("this js file is not working!");
+// let Dec, inc;
+// let info;
+// let width = 50;
+// document.addEventListener("scroll", () => {
+//     let scrollValue = window.scrollY;
+//     if (scrollValue) {
+//         Dec = setInterval(decrease, 10);
+//     }
+//     else if (scrollValue == 0) {
+//         inc = setInterval(increase, 2)
+//     }
+// })
+// const decrease = () => {
+//     // width = 50;
+//     if (width > 0 && info == "large") {
+//         width--;
+//         console.log(width);
+//         document.getElementById("logoImg").style.width = width + "%";
+//     }
+//     if (width == 0) {
+//         clearInterval(Dec);
+//         info = "Short";
+//     }
+// }
+
+// const increase = () => {
+//     width++;
+//     console.log(width);
+//     document.getElementById("logoImg").style.width = width + "%";
+//     if (width == 50) {
+//         clearInterval(inc);
+//         info = "large";
+//     }
+
+// }
